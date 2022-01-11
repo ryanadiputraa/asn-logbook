19b75aa5dc8bb180b3a5d3c60e52753bdea24e30
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-01-11 20:57:37 +0800

    setup material ui theme and app meta data

---
+++ src/lib/material-ui/theme.ts
@@ -0,0 +1,27 @@
+import { grey, indigo } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: indigo[200],
+    },
+    secondary: {
+      main: grey[50],
+    },
+  },
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
